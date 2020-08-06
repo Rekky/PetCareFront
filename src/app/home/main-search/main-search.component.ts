69920b47ca750828c56aaf8d4bd6230dcbb28319
Yvazz <luis.angel@anoopsystems.com>
2020-08-06 21:41:42 +0200

    adde new search maqueta

--- src/app/home/main-search/main-search.component.ts
+++ src/app/home/main-search/main-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ToolsService} from '../../../shared/services/tools.service';
 
 @Component({
   selector: 'app-main-search',
@@ -9,15 +10,21 @@ export class MainSearchComponent implements OnInit {
 
   currentSearchType: number = 0;
   bgImage: string = null;
+  tabSelected: number = 0;
 
-  constructor() { }
+  constructor(private tools: ToolsService) { }
 
   ngOnInit(): void {
     this.getRandomBg();
   }
 
+  search() {
+    console.log('entra search');
+  }
+
   getRandomBg() {
-    this.bgImage = '../assets/search_bg/pet-bg-1.jpg';
+    const randomNumber = this.tools.getRandomInt(1, 3);
+    this.bgImage = `../assets/search_bg/pet-bg-${randomNumber}.jpg`;
   }
 
   selectSearchType(type: number): void {
